5bf3b13d2ba3f6e5af264aec9b289b31f90c69f0
Worstsniper <goyalmukul2580@gmail.com>
2023-07-21 17:34:36 +0530

    feat: added comments

---
+++ src/components/healthQuestionnare/SeventhQuestionnare.jsx
@@ -0,0 +1,10 @@
+// this is the seventh question of drive 
+import React from 'react'
+
+const SeventhQuestionnare = () => {
+  return (
+    <div>SeventhQuestionnare</div>
+  )
+}
+
+export default SeventhQuestionnare
