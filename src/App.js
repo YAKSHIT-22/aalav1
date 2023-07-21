079b765ffe9bf262cceca68976fa9f487dee4ccf
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-07-21 16:05:52 +0530

    feat: added path selection page || route changes=d

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import CommunityPage from './pages/CommunityPage';
+import SelectionPage from './pages/SelectionPage';
 
 const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<LandingPage />} />
       <Route exact path="/community" element={<CommunityPage />} />
-      <Route exact path="/registration" element={<CommunityPage />} />
+      <Route exact path="/registration" element={<SelectionPage />} />
       <Route exact path="/registration/drive" element={<CommunityPage />} />
       <Route exact path="/registration/heath" element={<CommunityPage />} />
     </Routes>
