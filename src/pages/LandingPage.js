756aff9cfcdb2e8f59b782d91f512884198c7b4d
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-07-21 14:53:23 +0530

    feat: added community pages

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroSection from '../container/HeroSection';
 import AboutSection from '../container/AboutSection';
+import CommunitySection from '../container/CommunitySection';
 const LandingPage = () => {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <CommunitySection/>
     </>
   );
 };
