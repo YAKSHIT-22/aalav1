f87a803854ccbd135395ece194fedf1653e65605
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-07-21 21:33:12 +0530

    fix: removed unnecessary css

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -203,51 +203,51 @@ const Profile = () => {
               </li>
             </ul>
 
-            <nav aria-label="Pagination" className="mx-auto  D776PWUY">
-              <ul className="flex items-center gap-4 D776PWUY">
-                <li className="hidden sm:inline-block D776PWUY">
+            <nav aria-label="Pagination" className="mx-auto ">
+              <ul className="flex items-center gap-4">
+                <li className="hidden sm:inline-block">
                   <Link
                     href=""
-                    className="relative rounded-md w-10 h-10 flex items-center justify-center border border-transparent transition-colors duration-150 D776PWUY"
+                    className="relative rounded-md w-10 h-10 flex items-center justify-center border border-transparent transition-colors duration-150"
                     aria-current="page"
                   >
                     1
                   </Link>
                 </li>
-                <li className="hidden sm:inline-block D776PWUY">
+                <li className="hidden sm:inline-block">
                   <Link
                     href=""
-                    className="relative rounded-md w-10 h-10 flex items-center justify-center border border-transparent transition-colors duration-150 hover:border-white focus:border-white D776PWUY"
+                    className="relative rounded-md w-10 h-10 flex items-center justify-center border border-transparent transition-colors duration-150 hover:border-white focus:border-white"
                   >
                     2
                   </Link>
                 </li>
-                <li className="hidden sm:inline-block D776PWUY">
+                <li className="hidden sm:inline-block">
                   <Link
                     href=""
-                    className="relative rounded-md w-10 h-10 flex items-center justify-center border border-transparent transition-colors duration-150 hover:border-white focus:border-white D776PWUY"
+                    className="relative rounded-md w-10 h-10 flex items-center justify-center border border-transparent transition-colors duration-150 hover:border-white focus:border-white"
                   >
                     3
                   </Link>
                 </li>
-                <li className="hidden sm:inline-block D776PWUY">
+                <li className="hidden sm:inline-block">
                   <Link
                     href=""
-                    className="relative rounded-md w-10 h-10 flex items-center justify-center border border-transparent transition-colors duration-150 hover:border-white focus:border-white D776PWUY"
+                    className="relative rounded-md w-10 h-10 flex items-center justify-center border border-transparent transition-colors duration-150 hover:border-white focus:border-white"
                   >
                     4
                   </Link>
                 </li>
-                <li className="sm:hidden D776PWUY">
-                  <p aria-current="page" className="D776PWUY">
+                <li className="sm:hidden">
+                  <p aria-current="page" className=">
                     Page 1 of 4
                   </p>
                 </li>
-                <li className="next ease rounded-full border border-white transition-colors duration-150 D776PWUY">
+                <li className="next ease rounded-full border border-white transition-colors duration-150">
                   <Link
                     href=""
                     rel="prefetch"
-                    className="flex h-10 w-10 items-center justify-center D776PWUY"
+                    className="flex h-10 w-10 items-center justify-center"
                   >
                     <svg
                       data-hk="0-0"
@@ -257,7 +257,7 @@ const Profile = () => {
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                       aria-label="Go to page 2 of 4"
-                      className="D776PWUY "
+                      className= "
                     >
                       <path
                         d="M17.92 11.62C17.8724 11.4973 17.801 11.3851 17.71 11.29L12.71 6.29C12.6168 6.19676 12.5061 6.1228 12.3842 6.07234C12.2624 6.02188 12.1319 5.99591 12 5.99591C11.7337 5.99591 11.4783 6.1017 11.29 6.29C11.1968 6.38324 11.1228 6.49393 11.0723 6.61575C11.0219 6.73758 10.9959 6.86814 10.9959 7C10.9959 7.2663 11.1017 7.5217 11.29 7.71L14.59 11H7C6.73478 11 6.48043 11.1054 6.29289 11.2929C6.10536 11.4804 6 11.7348 6 12C6 12.2652 6.10536 12.5196 6.29289 12.7071C6.48043 12.8946 6.73478 13 7 13H14.59L11.29 16.29C11.1963 16.383 11.1219 16.4936 11.0711 16.6154C11.0203 16.7373 10.9942 16.868 10.9942 17C10.9942 17.132 11.0203 17.2627 11.0711 17.3846C11.1219 17.5064 11.1963 17.617 11.29 17.71C11.383 17.8037 11.4936 17.8781 11.6154 17.9289C11.7373 17.9797 11.868 18.0058 12 18.0058C12.132 18.0058 12.2627 17.9797 12.3846 17.9289C12.5064 17.8781 12.617 17.8037 12.71 17.71L17.71 12.71C17.801 12.6149 17.8724 12.5028 17.92 12.38C18.02 12.1365 18.02 11.8635 17.92 11.62Z"
