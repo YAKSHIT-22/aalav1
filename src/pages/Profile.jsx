e440030561b7ae68a0725ccefb2a67b8af913501
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-07-22 09:37:17 +0530

    feat: image added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -215,6 +215,8 @@ const Profile = () => {
                         June 22, 2023
                       </time>
                     </header>
+                    
+                    <img src="https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1275&q=80" width="1500" height="643" alt="" className="my-8" loading="lazy" decoding="async"/>
 
                     <div className="mt-6 grid gap-x-16 gap-y-4 md:grid-cols-2">
                       <h3 className="heading-4 md:heading-3">
